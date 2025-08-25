5a9d6a241a3d146a2e3f98c7361872949d8dce6d
Lara <Yollaraapustaja@gmail.com>
2025-08-25 13:49:01 -0300

    update readme

---
+++ goblin.py
@@ -0,0 +1,6 @@
+print("goblin")
+
+# Compiling
+To start compiling you must download python on your OS
+
+Next step is to install pip: ``python3 -m pip install -U discord.py`` and discord dependency
